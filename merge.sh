4243f0a423f09bd336e37aa6c3a7e3aebb12f09c
Sean Zhang <zbutfly@gmail.com>
2015-05-09 17:18:04 +0800

    revert encoding of adb, not support.

--- merge.sh
+++ merge.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -x
 
-base64 --decode -i butfly-adblock-plain.txt -o butfly-adblock.txt
-
 # refresh gfw-list, to be migrate to github.
 svn cat https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt > proxy-gfw.txt
 base64 --decode -i proxy-gfw.txt -o proxy-gfw-plain.txt
