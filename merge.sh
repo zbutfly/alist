d29c4f7e87ba60337bc730fd8805ba2847c88415
Sean Zhang <zbutfly@gmail.com>
2015-05-09 17:15:29 +0800

    encode adb list

--- merge.sh
+++ merge.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+base64 --decode -i butfly-adblock-plain.txt -o butfly-adblock.txt
+
 # refresh gfw-list, to be migrate to github.
 svn cat https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt > proxy-gfw.txt
 base64 --decode -i proxy-gfw.txt -o proxy-gfw-plain.txt
@@ -9,7 +11,7 @@ base64 --decode -i proxy-gfw.txt -o proxy-gfw-plain.txt
 base64 -i butfly-gfw-plain.txt -o butfly-gfw.txt
 
 #merge bf & gfw list.
-cat butfly-gfw-plain.txt  proxy-gfw-plain.txt  > butfly-merge-plain.txt
-base64 -i butfly-merge-plain.txt -o butfly-merge.txt
+cat butfly-gfw-plain.txt  proxy-gfw-plain.txt  > butfly-merge-gfw-plain.txt
+base64 -i butfly-merge-gfw-plain.txt -o butfly-merge-gfw.txt
 
 svn ci . -m "update revision of bf-list"
