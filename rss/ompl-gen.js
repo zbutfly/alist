fb3495e7ae6a6dd859871fa8f8d47338e971b40c
कलविङ्क <zhangx@hzcominfo.com>
2020-02-01 04:50:35 +0800

    add excel and json for rss

---
+++ rss/ompl-gen.js
@@ -0,0 +1,17 @@
+db.feeds.distinct("tags");
+var headed = false, footed = false;
+db.feeds.distinct("tags", {}).forEach(tag => {
+    if (!headed) {
+        headed = true;
+        print("<?xml version=\"1.0\" encoding=\"UTF-8\"?><opml version=\"1.0\"><head><title>Butfly's Subscriptions</title></head><body>");
+    }
+    print("<outline text=\""+ tag + "\" title=\"" + tag + "\">");
+    db.feeds.find({"tags": tag}).forEach(o => {
+        print("\t<outline type=\"rss\" text=\""+o.title+"\" title=\""+o.title+"\" xmlUrl=\""+o.xmlUrl+"\" htmlUrl=\""+o.htmlUrl+"\"/>");
+    });
+    print("</outline>");
+    if (!footed) {
+        footed = true;
+        print("</body></opml>");
+    }
+});
